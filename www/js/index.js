f1749742e88972ecc3d7d4abf9e140558dc6ce16
pk <pakolodziej@gmail.com>
2017-11-22 23:19:50 +0100

    a

--- www/js/index.js
+++ www/js/index.js
@@ -20,6 +20,12 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+		navigator.notification.alert(
+                'start',         // message
+                null,                 // callback
+                'debug',           // title
+                'Ok'                  // buttonName
+            );
     },
     // Bind Event Listeners
     //
@@ -33,15 +39,27 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+		navigator.notification.alert(
+                'device ready',         // message
+                null,                 // callback
+                'debug',           // title
+                'Ok'                  // buttonName
+            );
         console.log('Received Device Ready Event');
         console.log('calling setup push');
         app.setupPush();
     },
     setupPush: function() {
+		navigator.notification.alert(
+                'push init',         // message
+                null,                 // callback
+                'debug',           // title
+                'Ok'                  // buttonName
+            );
         console.log('calling push init');
         var push = PushNotification.init({
             "android": {
-                "senderID": "XXXXXXXX"
+                "senderID": "607120948857"
             },
             "browser": {},
             "ios": {
@@ -54,6 +72,12 @@ var app = {
         console.log('after init');
 
         push.on('registration', function(data) {
+		navigator.notification.alert(
+                data.registrationId,         // message
+                null,                 // callback
+                'debug',           // title
+                'Ok'                  // buttonName
+            );
             console.log('registration event: ' + data.registrationId);
 
             var oldRegId = localStorage.getItem('registrationId');
